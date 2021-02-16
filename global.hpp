03a8ab209424435e63c80730eebc405fb97d6b60
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-02-16 16:44:52 -0500

    fix typo

--- global.hpp
+++ global.hpp
@@ -9,7 +9,7 @@ inline uintptr_t _MODULE_HANDLE = reinterpret_cast<uintptr_t>(GetModuleHandle(0)
 // game manager
 inline uintptr_t _GAME_MANAGER = _MODULE_HANDLE + 0x3222D0;
 
-// menu later
+// menu layer
 inline uintptr_t _MENU_LAYER = _MODULE_HANDLE + 0x1907B0;
 
 const char* cinnamon_version()

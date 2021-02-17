fd592ed3a75111e52c5df185df5833e55bc39d70
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-02-16 20:47:56 -0500

    make it std::string as const char* is broken

--- lib/process.cpp
+++ lib/process.cpp
@@ -3,12 +3,12 @@
 
 namespace cinnamon::process
 {
-	const char* current_directory()
+	std::string current_directory()
 	{
 		char buffer[MAX_PATH];
 		GetModuleFileNameA(NULL, buffer, MAX_PATH);
 		std::string::size_type pos = std::string(buffer).find_last_of("\\/");
 
-		return std::string(buffer).substr(0, pos).c_str();
+		return std::string(buffer).substr(0, pos);
 	}
 }
